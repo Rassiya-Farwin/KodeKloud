cbf9d4326d22a13061fd4b75cefa5b812581e99d
Rassiya <111345313+Rassiya-Farwin@users.noreply.github.com>
2025-10-26 02:02:45 +0530

    Delete 100dayaOfDevops directory

--- 100dayaOfDevops/day68/installation.sh
+++
@@ -1,8 +0,0 @@
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-# Add required dependencies for the jenkins package
-sudo yum install fontconfig java-21-openjdk
-sudo yum install jenkins
-sudo systemctl daemon-reload
